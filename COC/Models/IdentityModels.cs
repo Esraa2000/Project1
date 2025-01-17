ea2d79d372bdd77e5c1c07ef1aab27706de8920f
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-01-17 19:22:28 +0200

    Update IdentityModels.cs

--- COC/Models/IdentityModels.cs
+++ COC/Models/IdentityModels.cs
@@ -1,16 +1,12 @@
-﻿
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 
 using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace COC.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    
     public class ApplicationUser : IdentityUser
     {
         
