8b03ea767174b74bf77a2daac7a96d296f341d21
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-01-17 19:30:06 +0200

    Update Users_in_Role_ViewModel.cs

--- COC/Models/Users_in_Role_ViewModel.cs
+++ COC/Models/Users_in_Role_ViewModel.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
 namespace COC.Models
 {
-    public class Users_in_Role_ViewModel
+    public class UsersinRoleViewModel
     {
        
     public string UserId { get; set; }
