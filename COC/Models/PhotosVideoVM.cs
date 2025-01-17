656f07472eb08a771a76726097c19a24f55ed938
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-01-17 19:24:37 +0200

    Update PhotosVideoVM.cs

--- COC/Models/PhotosVideoVM.cs
+++ COC/Models/PhotosVideoVM.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-
 namespace COC.Models
 {
-    using System;
-    using System.Collections.Generic;
     public class PhotosVideoVM
     {
 
