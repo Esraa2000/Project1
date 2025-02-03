ec1f2c83f5d886a0a37fbb98c88024ef79319594
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-02-03 21:06:34 +0200

    Update HomeController.cs

--- COC/Controllers/HomeController.cs
+++ COC/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Microsoft.AspNetCore.Mvc;
 using COC.Models;
 using Microsoft.EntityFrameworkCore;
@@ -13,84 +13,84 @@ namespace COC.Controllers
         private QUDBContext db = new QUDBContext();
         private identityDbContext _identityDb = new identityDbContext();
 
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
             
             AllData obj = new AllData();
 
-            obj.Newsobj = db.News.ToList();
-            obj.Eventobj = db.Events.ToList();
-            obj.photosobj = db.PhotosVideos.ToList();
-            obj.DiscoverObj = db.Discovers.ToList();
-            obj.MainMenuObj = db.MainMenus.ToList();
-            obj.SubMenuObj=db.SubMenus.ToList();
+            obj.Newsobj = await db.News.ToListAsync();
+            obj.Eventobj = await db.Events.ToListAsync();
+            obj.photosobj = await db.PhotosVideos.ToListAsync();
+            obj.DiscoverObj = await db.Discovers.ToListAsync();
+            obj.MainMenuObj = await db.MainMenus.ToListAsync();
+            obj.SubMenuObj=await db.SubMenus.ToListAsync();
             return View(obj);
         }
 
-        public ActionResult GetMenu()
+        public async Task<IActionResult> GetMenu()
         {
             
             AllData obj = new AllData();
 
-            obj.MainMenuObj = db.MainMenus.Include("SubMenu").ToList();
-            obj.SocialObj = db.Socials.FirstOrDefault();
+            obj.MainMenuObj = await db.MainMenus.Include("SubMenu").ToListAsync();
+            obj.SocialObj = await db.Socials.FirstOrDefaultAsync();
 
             return PartialView(obj);
         }
 
-        public ActionResult GetFooter()
+        public async Task<IActionResult> GetFooter()
         {
             AllData obj = new AllData();
 
             
-            obj.MainMenuObj = db.MainMenus.Include("SubMenu").ToList();
+            obj.MainMenuObj = await db.MainMenus.Include("SubMenu").ToListAsync();
             return PartialView(obj);
         }
 
-        public ActionResult NewsDetails(int newsid)
+        public async Task<IActionResult> NewsDetails(int newsid)
         {
-            var newsdetails = db.News.Find(newsid);
+            var newsdetails = await db.News.FindAsync(newsid);
 
 
             return View(newsdetails);
         }
 
-        public ActionResult DiscoverDetails(int Discid)
+        public async Task<IActionResult> DiscoverDetails(int Discid)
         {
-            var discdetails = db.Discovers.Find(Discid);
+            var discdetails = await db.Discovers.FindAsync(Discid);
             return View(discdetails);
         }
 
-        public ActionResult SubMenuDetails(int SubMid)
+        public async Task<IActionResult> SubMenuDetails(int SubMid)
         {
-            var submenudetails = db.SubMenus.Find(SubMid);
+            var submenudetails = await db.SubMenus.FindAsync(SubMid);
 
             return View(submenudetails);
         }
 
-        public ActionResult EventDetails(int Eventid)
+        public async Task<IActionResult> EventDetails(int Eventid)
         {
-            var eventdetails = db.Events.Find(Eventid);
+            var eventdetails = await db.Events.FindAsync(Eventid);
             return View(eventdetails);
         }
 
         [HttpPost]
-        public ActionResult SearchData(AllData alldata)
+        public async Task<IActionResult> SearchData(AllData alldata)
         {
-            var data = db.News.Where(news => news.Title.Contains(alldata.search)).ToList();
+            var data = await db.News.Where(news => news.Title.Contains(alldata.search)).ToListAsync();
 
             return View(data);
         }
 
         [HttpPost]
-        public ActionResult About()
+        public IActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public IActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
