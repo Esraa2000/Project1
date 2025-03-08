66fe05d65a4992f9ca69dd72c192e68e1ba5aebc
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-03-09 01:31:43 +0200

    Add files via upload

---
+++ COC/Repositories/DiscoverRepository.cs
@@ -0,0 +1,54 @@
+﻿using COC.Models;
+using COC.ModelDB.QUDB;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace COC.Repositories
+{
+    public class DiscoverRepository : IDiscoverRepository
+    {
+        private readonly QUDBContext _context;
+
+        public DiscoverRepository(QUDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Discover>> GetAll()
+        {
+            return await _context.Discovers.ToListAsync();
+        }
+
+
+        public async Task<Discover> GetById(int id)
+        {
+            return await _context.Discovers.FindAsync(id);
+        }
+        public async Task Add(Discover discover)
+        {
+            await _context.Discovers.AddAsync(discover);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task Update(Discover discover)
+        {
+            _context.Discovers.Update(discover);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var item = await _context.Discovers.FindAsync(id);
+            if (item != null)
+            {
+                _context.Discovers.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+    
+}
---
+++ COC/Repositories/IDiscoverRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using COC.ModelDB.QUDB;
+using COC.Models;
+namespace COC.Repositories
+{
+    public interface IDiscoverRepository
+    {
+        Task<IEnumerable<Discover>> GetAll();
+        Task<Discover> GetById(int id);
+        Task Add (Discover discover);
+        Task Update(Discover discover);
+        Task Delete(int id);
+    }
+}
